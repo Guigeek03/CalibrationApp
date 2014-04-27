d715752688d108f98a3f5051229cf02e38fbf632
nemeo999 <pdestouchesstrg@gmail.com>
2014-04-27 11:34:25 +0200

    ActionBar changes

--- gen/fr/utbm/calibrationapp/R.java
+++ gen/fr/utbm/calibrationapp/R.java
@@ -11,11 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int background=0x7f040004;
-        public static final int buttonback=0x7f040002;
-        public static final int buttonbackpressed=0x7f040003;
-        public static final int rights=0x7f040000;
-        public static final int white=0x7f040001;
+        public static final int actionbar=0x7f050003;
+        public static final int background=0x7f050006;
+        public static final int black=0x7f050000;
+        public static final int buttonback=0x7f050004;
+        public static final int buttonbackpressed=0x7f050005;
+        public static final int rights=0x7f050002;
+        public static final int white=0x7f050001;
     }
     public static final class drawable {
         public static final int h_rdc=0x7f020000;
@@ -56,23 +58,23 @@ public final class R {
         public static final int menu_calibration=0x7f070002;
     }
     public static final class string {
-        public static final int act_buildings=0x7f050001;
-        public static final int act_calibration=0x7f050003;
-        public static final int act_floors=0x7f050002;
-        public static final int actionAdd=0x7f05000c;
-        public static final int actionDiscard=0x7f05000d;
-        public static final int actionRefresh=0x7f05000e;
-        public static final int action_settings=0x7f050005;
-        public static final int app_name=0x7f050000;
-        public static final int but_calibration=0x7f050006;
-        public static final int desc_logo=0x7f050008;
-        public static final int from=0x7f05000f;
-        public static final int hello_world=0x7f050004;
-        public static final int pref_title_server_address=0x7f05000a;
-        public static final int pref_title_server_name=0x7f050009;
-        public static final int pref_title_server_port=0x7f05000b;
-        public static final int settings=0x7f050007;
-        public static final int welcome=0x7f050010;
+        public static final int act_buildings=0x7f060001;
+        public static final int act_calibration=0x7f060003;
+        public static final int act_floors=0x7f060002;
+        public static final int actionAdd=0x7f06000c;
+        public static final int actionDiscard=0x7f06000d;
+        public static final int actionRefresh=0x7f06000e;
+        public static final int action_settings=0x7f060005;
+        public static final int app_name=0x7f060000;
+        public static final int but_calibration=0x7f060006;
+        public static final int desc_logo=0x7f060008;
+        public static final int from=0x7f06000f;
+        public static final int hello_world=0x7f060004;
+        public static final int pref_title_server_address=0x7f06000a;
+        public static final int pref_title_server_name=0x7f060009;
+        public static final int pref_title_server_port=0x7f06000b;
+        public static final int settings=0x7f060007;
+        public static final int welcome=0x7f060010;
     }
     public static final class style {
         /** 
@@ -96,10 +98,19 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f040003;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f040004;
+        /**  the theme applied to the application or activity 
+         */
+        public static final int CustomActionBarTheme=0x7f040000;
+        /**  ActionBar styles 
+         */
+        public static final int MyActionBar=0x7f040001;
+        /**  ActionBar title text 
+         */
+        public static final int MyActionBarTitleText=0x7f040002;
     }
 }
