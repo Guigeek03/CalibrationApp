8cc2c2c527cafe1f173040d6a5e48ef50ef22301
Guigeek03 <guillaume.rochelle@gmail.com>
2014-04-30 17:29:10 +0200

    Check IP address and port number

--- src/fr/utbm/calibrationapp/PrefsActivity.java
+++ src/fr/utbm/calibrationapp/PrefsActivity.java
@@ -1,18 +1,21 @@
 package fr.utbm.calibrationapp;
 
+import java.util.regex.Pattern;
+
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
 import android.view.MenuItem;
 
-public class PrefsActivity extends PreferenceActivity implements
-		OnSharedPreferenceChangeListener {
+public class PrefsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,25 +30,55 @@ public class PrefsActivity extends PreferenceActivity implements
 		editServerName.setSummary(sp.getString("serverName", "Default server"));
 		editServerAddress.setSummary(sp.getString("serverAddress", "0.0.0.0"));
 		editServerPort.setSummary(sp.getString("serverPort", "80"));
+
+		findPreference("serverAddress").setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				if (!Pattern.matches("^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$", (String) newValue)) {
+					displayModal("Invalid IP address", "Enter an IP address with the following format :"
+							+ "\n[0-255].[0-255].[0-255].[0-255]");
+					return false;
+				}
+
+				return true;
+			}
+		});
+		
+		findPreference("serverPort").setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				try {
+					int port = Integer.parseInt((String) newValue);
+					if (port < 0 || port > 65536) {
+						displayModal("Invalid port number", "Enter a port number between 0 and 65536 !");
+						return false;
+					}
+				} catch (NumberFormatException e) {
+					displayModal("Invalid port number", "Enter a port number between 0 and 65536 !");
+					return false;
+				}
+
+				return true;
+			}
+		});
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		getPreferenceScreen().getSharedPreferences()
-				.unregisterOnSharedPreferenceChangeListener(this);
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		getPreferenceScreen().getSharedPreferences()
-				.registerOnSharedPreferenceChangeListener(this);
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-			String key) {
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 		Preference pref = findPreference(key);
 		if (pref instanceof EditTextPreference) {
 			EditTextPreference etp = (EditTextPreference) pref;
@@ -67,5 +100,13 @@ public class PrefsActivity extends PreferenceActivity implements
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}	
+	}
+	
+	public void displayModal(String title, String error) {
+		final AlertDialog.Builder builder = new AlertDialog.Builder(PrefsActivity.this);
+		builder.setTitle(title);
+		builder.setMessage(error);
+		builder.setPositiveButton(android.R.string.ok, null);
+		builder.show();
+	}
 }
